459d35610e2cf64ba38de8b36865bb56fc68ebf4
sobotab <sobotab@udel.edu>
2021-10-18 21:32:22 -0400

    added server udp functions

--- udp.c
+++ udp.c
@@ -11,7 +11,20 @@ ret_packet_t *makePacket(unsigned short req_id, unsigned short seq_num, unsigned
 	return new_packet;
 }
 
-ret_packet_t **makeMessage(unsigned int req_id, unsigned int count) {
+ret_packet_t **makeMessage(unsigned short req_id, unsigned short count) {
+	short remaining_count=count;
+	short count_arg=25;
+	int last=0;
+	i=1;
 	ret_packet_t **new_message=malloc(sizeof(ret_packet)*(count/25+1));
-	while (count) {
-		makePacket(req_id, i, last_bool, count); //fix this and create values
+	while (!last && remaining_count) {
+		if(remaining_count > 25) {
+			remaining_count=remaining_count-count_arg;
+		} else {
+			last = 1;
+			count_arg=remaining_count;
+		}
+		makePacket(req_id, i++, last, count_arg);
+	}
+	return new_message;
+}
